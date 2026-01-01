1f9f01c99900eb100e1ea3a56e810b32056910cb
wy000000 <vw0000@126.com>
2026-01-02 01:29:28 +0800

    addressTF处理完成，待展平

--- addressTimeDataClass.py
+++ addressTimeDataClass.py
@@ -2,41 +2,81 @@ from BTNHGV2ParameterClass import BTNHGV2ParameterClass
 from addressTimeFeatureClass import addressTimeFeatureClass
 import pandas as pd
 import os
+import time
 
 class addressTimeDataClass:
+	# addressCount=546649
 	def __init__(self,
 		dataPath=BTNHGV2ParameterClass.dataPath):
 		self._dataPath=dataPath
-		self.addressTime_feat_df=self._loadAddressTimeFeature()
-		#定义一个addressTimeFeatureClass类型的list
-		self.addressTimeFeatureCls_list=[]
+		self.addressTime_data_df=self._loadAddressTimeData()
+
+		# 初始化 address_dict 为空字典
+		# 键：addressID
+		# 值：包含 clusterID 和 addressTimeFeatureCls 的字典
+		self.address_dict = {}
+		self.processAddressTimeData()
+		#将address_dict中的addressTimeFeatureCls.block_features展平追加到address_dict,
+		#保留block_features各项的名称
+		
+		
+		
+		# self.address_time_feature_dict = {}
 	
-	def _loadAddressTimeFeature(self, dataPath:str=None)->pd.DataFrame:
+	def _loadAddressTimeData(self, dataPath:str=None)->pd.DataFrame:
 		print("start read addressTimeData")
 		if dataPath is None:
 			dataPath=self._dataPath
 		#读取所有数据
-		addressTime_feat_df = pd.read_csv(os.path.join(dataPath, "addressTimeData.csv"))
-		return addressTime_feat_df
+		addressTime_data_df = pd.read_csv(os.path.join(dataPath, "addressTimeData.csv"))
+		print("读取完成。addressTime_data_df.shape=",addressTime_data_df.shape)
+		return addressTime_data_df
 	
-	def processAddressTimeFeature(self):
-		#取self.address_time_feat_df的长度
-		length=self.address_time_feat_df.shape[0]
-		i=0
-		while i<length:
-			addressTimeFeatureCls=addressTimeFeatureClass(self.addressTime_feat_df, i)
-			i=addressTimeFeatureCls.getAddressTimeFeature()
-			
-
-			
-
-	
-atd=addressTimeDataClass()
-			
-
-	
-
-
+	# 使用itertuples()代替iterrows()，速度更快
+	def processAddressTimeData(self):
+		time1 = time.time()
+		rowCount = self.addressTime_data_df.shape[0]
 		
+		# 使用字典存储addressTimeFeatureCls实例
+		address_dict = self.address_dict		
+		for i, row_tuple in enumerate(self.addressTime_data_df.itertuples(index=False)):
+			addressID = row_tuple.addressID
+			clusterID = row_tuple.clusterID
+			
+			if addressID not in address_dict:
+				# 创建新实例并存储到字典
+				addressTimeFeatureCls = addressTimeFeatureClass(row_tuple)
+				addressTimeFeatureCls.process_address_time_features(row_tuple)
+				address_dict[addressID] = {
+					"clusterID": clusterID,
+					"addressTimeFeatureCls": addressTimeFeatureCls
+				}
+			else:
+				# 更新现有实例
+				addressTimeFeatureCls = address_dict[addressID]["addressTimeFeatureCls"]
+				addressTimeFeatureCls.process_address_time_features(row_tuple)
+			if (i+1)%10001==0:
+				print("已完成{}行，进度{:.2f}%".format(i, i/rowCount*100))
+				# break
+		print("更新差异特征")
+		for addressID in address_dict:
+			addressTimeFeatureCls = address_dict[addressID]["addressTimeFeatureCls"]
+			addressTimeFeatureCls.update_diff_features()
+		
+		time2=time.time()
+		print("时间特性处理完成。address_dict长度=",len(address_dict))
+		# 输出处理时间 时：分：秒
+		print("时间特性处理耗时：{}时{}分{}秒".format(int((time2-time1)//3600), int((time2-time1)//60), int((time2-time1)%60)))
+		#输出当前时间
+		print("当前时间：{}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+	
+		# #输出self.address_dict前64个元素
+		# print(list(self.address_dict.items())[:64])
+		return address_dict
+	
+
+	
+	
 
 
+atd=addressTimeDataClass()
--- addressTimeFeatureClass.py
+++ addressTimeFeatureClass.py
@@ -1,67 +1,101 @@
 import pandas as pd
 import numpy as np
 from BTNHGV2ParameterClass import BTNHGV2ParameterClass
+from typing import Any
 
 class addressTimeFeatureClass:
-	minBlockID=272375
-	maxBlockID=277995
-	spanOfBlocks=maxBlockID-minBlockID+1
-	addressBlockFeatureCount=13
-
-	def __init__(self
-				, address_time_feat_df:pd.DataFrame
-				, rowNo:int):		
-		self.addressID=address_time_feat_df.loc[rowNo, "addressID"]
-		self.clusterID=address_time_feat_df.loc[rowNo, "clusterID"]		
-		blockFeatureDict = { "blockID": -1, "coinCount": 0, "coinAmount": 0, "inCoinCount": 0,\
-							"inCoinAmount": 0, "outCoinCount": 0, "outCoinAmount": 0, "diffCoinCount": 0,\
-							"diffCoinAmount": 0, "txCount": 0, "inTxCount": 0, "outTxCount": 0,\
-							"diffTxCount": 0 }
-		# 创建 DataFrame
-		self.blockFeatureDF = pd.DataFrame([blockFeatureDict]*self.spanOfBlocks)		
+	#region
+	minBlockID = 272375
+	maxBlockID = 277995
+	spanOfBlocks = maxBlockID - minBlockID + 1
+	addressBlockFeatureCount = 13
 	
-	def process_address_time_features(self
-								, address_time_feat_df:pd.DataFrame
-								, rowNo:int):
-		self.process_inTx(address_time_feat_df, rowNo)
-		self.process_outTx(address_time_feat_df, rowNo)
+	# 定义特征名称和对应的索引
+	FEATURE_NAMES = [
+		"blockID", "coinCount", "coinAmount", "inCoinCount", "inCoinAmount",
+		"outCoinCount", "outCoinAmount", "diffCoinCount", "diffCoinAmount",
+		"txCount", "inTxCount", "outTxCount", "diffTxCount"]
+	
+	# 预计算特征索引，提高访问速度
+	BLOCK_ID_IDX = FEATURE_NAMES.index("blockID")
+	COIN_COUNT_IDX = FEATURE_NAMES.index("coinCount")
+	COIN_AMOUNT_IDX = FEATURE_NAMES.index("coinAmount")
+	IN_COIN_COUNT_IDX = FEATURE_NAMES.index("inCoinCount")
+	IN_COIN_AMOUNT_IDX = FEATURE_NAMES.index("inCoinAmount")
+	OUT_COIN_COUNT_IDX = FEATURE_NAMES.index("outCoinCount")
+	OUT_COIN_AMOUNT_IDX = FEATURE_NAMES.index("outCoinAmount")
+	DIF_COIN_COUNT_IDX = FEATURE_NAMES.index("diffCoinCount")
+	DIF_COIN_AMOUNT_IDX = FEATURE_NAMES.index("diffCoinAmount")	
+	TX_COUNT_IDX = FEATURE_NAMES.index("txCount")
+	IN_TX_COUNT_IDX = FEATURE_NAMES.index("inTxCount")
+	OUT_TX_COUNT_IDX = FEATURE_NAMES.index("outTxCount")
+	DIF_TX_COUNT_IDX = FEATURE_NAMES.index("diffTxCount")
 
-	def process_inTx(self
-					, address_time_feat_df:pd.DataFrame
-					, rowNo:int):
-		minBlockID=addressTimeFeatureClass.minBlockID
-		#address_time_feat_df是原始数据，处理当前行，rowNo为当前行号
-		inBlockID=address_time_feat_df.iloc[rowNo, "inBlockID"]
-		# inBlockID is nan
+	#endregion
+
+	def __init__(self, row: Any):
+		self.addressID = row.addressID
+		self.clusterID = row.clusterID
+		
+		# 使用 NumPy 数组替代 DataFrame，初始化所有值为0
+		# 形状：(spanOfBlocks, addressBlockFeatureCount)
+		self.block_features = np.zeros((self.spanOfBlocks, self.addressBlockFeatureCount), dtype=np.float64)
+		
+		# 初始化 blockID 为 -1
+		self.block_features[:, self.BLOCK_ID_IDX] = -1
+
+	def process_address_time_features(self, row: Any):
+		self._process_inTx(row)
+		self._process_outTx(row)
+		return
+
+	def _process_inTx(self, row: Any):
+		inBlockID = row.inBlockID
 		if pd.isna(inBlockID):
 			return False
-		value=address_time_feat_df.loc[rowNo, "value"]
-		#更新address的inBlockID的block数据
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "blockID"]=inBlockID
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "coinCount"]+=1
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "coinAmount"]+=value
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "inCoinCount"]+=1
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "inCoinAmount"]+=value
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "txCount"]+=1
-		self.blockFeatureDF.loc[inBlockID-minBlockID, "inTxCount"]+=1
+			
+		value = row.value
+		idx = int(inBlockID - self.minBlockID)
+		
+		# 更新特征数组（比 DataFrame.loc 快得多）
+		self.block_features[idx, self.BLOCK_ID_IDX] = inBlockID
+		self.block_features[idx, self.COIN_COUNT_IDX] += 1
+		self.block_features[idx, self.COIN_AMOUNT_IDX] += value
+		self.block_features[idx, self.IN_COIN_COUNT_IDX] += 1
+		self.block_features[idx, self.IN_COIN_AMOUNT_IDX] += value
+		self.block_features[idx, self.TX_COUNT_IDX] += 1
+		self.block_features[idx, self.IN_TX_COUNT_IDX] += 1
 		return True
-	
-	def process_outTx(self
-					, address_time_feat_df:pd.DataFrame
-					, rowNo:int):
-		minBlockID=addressTimeFeatureClass.minBlockID
-		#address_time_feat_df是原始数据，处理当前行，rowNo为当前行号		
-		outBlockID=address_time_feat_df.loc[rowNo, "outBlockID"]
-		# outBlockID is nan
+
+	def _process_outTx(self, row: Any):
+		outBlockID = row.outBlockID
 		if pd.isna(outBlockID):
 			return False
-		value=address_time_feat_df.loc[rowNo, "value"]
-		#更新address的outBlockID的block数据
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "blockID"]=outBlockID
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "coinCount"]+=1
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "coinAmount"]+=value
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "outCoinCount"]+=1
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "outCoinAmount"]+=value
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "txCount"]+=1
-		self.blockFeatureDF.loc[outBlockID-minBlockID, "outTxCount"]+=1
+			
+		value = row.value
+		idx = int(outBlockID - self.minBlockID)
+		
+		# 更新特征数组
+		self.block_features[idx, self.BLOCK_ID_IDX] = outBlockID
+		self.block_features[idx, self.COIN_COUNT_IDX] += 1
+		self.block_features[idx, self.COIN_AMOUNT_IDX] += value
+		self.block_features[idx, self.OUT_COIN_COUNT_IDX] += 1
+		self.block_features[idx, self.OUT_COIN_AMOUNT_IDX] += value
+		self.block_features[idx, self.TX_COUNT_IDX] += 1
+		self.block_features[idx, self.OUT_TX_COUNT_IDX] += 1
 		return True
+	
+	def update_diff_features(self):
+		"""更新差异特征（在所有块处理完成后调用）"""
+		# 使用NumPy向量运算替代Python循环，速度提升显著
+		self.block_features[:, self.DIF_COIN_COUNT_IDX] = (
+				self.block_features[:, self.IN_COIN_COUNT_IDX] - 
+				self.block_features[:, self.OUT_COIN_COUNT_IDX])
+		
+		self.block_features[:, self.DIF_COIN_AMOUNT_IDX] = (
+				self.block_features[:, self.IN_COIN_AMOUNT_IDX] - 
+				self.block_features[:, self.OUT_COIN_AMOUNT_IDX])
+		
+		self.block_features[:, self.DIF_TX_COUNT_IDX] = (
+				self.block_features[:, self.IN_TX_COUNT_IDX] - 
+				self.block_features[:, self.OUT_TX_COUNT_IDX])
